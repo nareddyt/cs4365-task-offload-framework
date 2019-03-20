25e6f19932853194680e35583307ae35f0fa99fd
Tejasvi Nareddy <tejunareddy@gmail.com>
2019-03-20 01:10:34 -0400

    Reduce image size

--- ball-tracking-example/sequential.py
+++ ball-tracking-example/sequential.py
@@ -30,7 +30,7 @@ while True:
 
     # resize the frame, blur it, and convert it to the HSV
     # color space
-    frame = imutils.resize(frame, width=600)
+    frame = imutils.resize(frame, width=300)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
