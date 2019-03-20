dff8a648c16b563c68d67d5dbfe743e814ce38e5
Tejasvi Nareddy <tejunareddy@gmail.com>
2019-03-20 00:55:35 -0400

    Simplify by removing tail tracking (per-frame processing only now)

--- ball-tracking-example/sequential.py
+++ ball-tracking-example/sequential.py
@@ -1,12 +1,9 @@
 
 # import the necessary packages
-from collections import deque
-import numpy as np
 import cv2
 import imutils
 import time
 
-TAIL_SIZE = 64
 FPS_AVG_WINDOW = 5
 
 # define the lower and upper boundaries of the "green"
@@ -14,7 +11,6 @@ FPS_AVG_WINDOW = 5
 # list of tracked points
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
-pts = deque(maxlen=TAIL_SIZE)
 
 # grab a reference to the video file
 vs = cv2.VideoCapture("./ball_tracking_example.mp4")
@@ -47,17 +43,17 @@ while True:
 
     # find contours in the mask and initialize the current
     # (x, y) center of the ball
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                            cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+    contours = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+                                cv2.CHAIN_APPROX_SIMPLE)
+    contours = imutils.grab_contours(contours)
     center = None
 
     # only proceed if at least one contour was found
-    if len(cnts) > 0:
+    if len(contours) > 0:
         # find the largest contour in the mask, then use
         # it to compute the minimum enclosing circle and
         # centroid
-        c = max(cnts, key=cv2.contourArea)
+        c = max(contours, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
         center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
@@ -69,21 +65,6 @@ while True:
             cv2.circle(frame, (int(x), int(y)), int(radius),
                        (0, 255, 255), 2)
             cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-    # update the points queue
-    pts.appendleft(center)
-
-    # loop over the set of tracked points
-    for i in range(1, len(pts)):
-        # if either of the tracked points are None, ignore
-        # them
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-
-        # otherwise, compute the thickness of the line and
-        # draw the connecting lines
-        thickness = int(np.sqrt(TAIL_SIZE / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
 
     # show the frame to our screen
     cv2.imshow("Frame", frame)
