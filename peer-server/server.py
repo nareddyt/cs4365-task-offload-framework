325e41c25acb5bc77eb1ef203be309bfb89f5498
hyemme3 <hyemme3@gatech.edu>
2019-03-29 14:56:12 -0400

    Added peer server code

---
+++ peer-server/server.py
@@ -0,0 +1,38 @@
+import socket
+import sys
+import cv2
+import pickle
+import numpy as np
+import struct
+
+HOST=''
+PORT=8089
+
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+print('Socket created')
+
+s.bind((HOST,PORT))
+print('Socket bind complete')
+s.listen(10)
+print('Socket now listening')
+
+conn,addr=s.accept()
+
+### new
+data = ""
+payload_size = struct.calcsize("H")
+while True:
+    while len(data) < payload_size:
+        data += conn.recv(4096)
+    packed_msg_size = data[:payload_size]
+    data = data[payload_size:]
+    msg_size = struct.unpack("H", packed_msg_size)[0]
+    while len(data) < msg_size:
+        data += conn.recv(4096)
+    frame_data = data[:msg_size]
+    data = data[msg_size:]
+    ###
+
+    frame=pickle.loads(frame_data)
+    print(frame)
+    cv2.imshow('frame',frame)
